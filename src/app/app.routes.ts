ad7b33f0a32bd525af26548a72376e2ea0ebe8a1
Martin Stanek <martin.stanek@logicline.de>
2025-01-21 17:02:08 +0100

    added basic functionality and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { QuizPageComponent } from './quiz-page/quiz-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+  {
+    path: '',
+    component: MainPageComponent,
+  },
+  {
+    path: 'quiz',
+    component: QuizPageComponent,
+  },
+];
---
+++ src/app/question/question.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import question from '../../assets/questions.json'
+
+@Component({
+  selector: 'app-question',
+  standalone: true,
+  imports: [],
+  templateUrl: './question.component.html',
+  styleUrl: './question.component.css'
+})
+export class QuestionComponent {
+}
