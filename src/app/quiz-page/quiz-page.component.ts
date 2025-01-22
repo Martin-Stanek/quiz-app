ec1c0df1261cb552887273dcc32d0e40802f9aa1
Martin Stanek <martin.stanek@logicline.de>
2025-01-22 10:37:01 +0100

    added timer

--- src/app/quiz-page/quiz-page.component.ts
+++ src/app/quiz-page/quiz-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import {interval} from 'rxjs';
 
 @Component({
   selector: 'app-quiz-page',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './quiz-page.component.html',
   styleUrl: './quiz-page.component.css'
 })
-export class QuizPageComponent {
+export class QuizPageComponent implements OnInit{
+  
+  private destroyRef = inject(DestroyRef);
+  timervalue: number=0;
 
+  ngOnInit(): void{
+    const subscription = interval(1000).subscribe({
+      next: (val) => this.timervalue = val
+        });
+        this.destroyRef.onDestroy(() => {
+          subscription.unsubscribe();
+        })
+  }
 }
